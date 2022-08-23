c0913a0d653b7b1b8e3cf4279ef7aef13e3ffbf5
ShowunmiAdeogo <showunmi2017@gmail.com>
2022-08-23 00:45:33 -0700

    alien_invasion.py

---
+++ ship.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Ship(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/player.png')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left =  False
+
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
+
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
